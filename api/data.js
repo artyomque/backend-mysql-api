465cce1bce1551726b1ea5722cf6aa9327025e69
Artyom Sabura <sabura95@gmail.com>
2025-03-25 14:23:29 +0300

    del express

--- api/data.js
+++ api/data.js
@@ -1,10 +1,7 @@
-import express from "express";
 import mysql from "mysql2/promise";
 
 export default async function handler(req, res) {
-  const app = express();
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://users-panel-react.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
@@ -12,7 +9,7 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  app.get("/api/data", async (req, res) => {
+  try {
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
@@ -20,16 +17,11 @@ export default async function handler(req, res) {
       database: process.env.DB_DATABASE,
     });
 
-    try {
-      const [rows] = await connection.execute("SELECT * FROM users LIMIT 50");
-      res.status(200).json(rows);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Database query failed" });
-    } finally {
-      connection.end();
-    }
-  });
-
-  app(req, res);
+    const [rows] = await connection.execute("SELECT * FROM users LIMIT 50");
+    res.status(200).json(rows);
+    await connection.end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database query failed" });
+  }
 }
